d8423a3bae05cf91351b9456f417f71c58174bc7
rawwad97 <117926064+rawwad97@users.noreply.github.com>
2022-11-14 23:52:26 -0600

    Added the search engine

--- src/index.js
+++ src/index.js
@@ -42,9 +42,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "c416c42fca4507b10b7e33866fc59337";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let city = "New York";
-let apiKey = "c416c42fca4507b10b7e33866fc59337";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
