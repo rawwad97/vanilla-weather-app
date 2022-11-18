67ac8853171f07fa9743774cfa9ab08124516ff6
rawwad97 <117926064+rawwad97@users.noreply.github.com>
2022-11-17 21:37:35 -0600

    removed console.log

--- src/index.js
+++ src/index.js
@@ -65,7 +65,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
